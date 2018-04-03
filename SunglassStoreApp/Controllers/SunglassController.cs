d28fee5db1a59687d453dbbafcb47045b65b4147
Sadat Abdulla Al Muniem <sadatjnu9@gmail.com>
2018-04-03 22:28:01 +0600

    Add files via upload

---
+++ SunglassStoreApp/Controllers/SunglassController.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SunglassStoreApp.Controllers
+{
+    public class SunglassController : Controller
+    {
+        // GET: Sunglass
+        public ActionResult MainBody()
+        {
+            return View();
+        }
+        public ActionResult MenProducts()
+        {
+            return View();
+        }
+        public ActionResult Account()
+        {
+            return View();
+        }
+        public ActionResult KidsProducts()
+        {
+            return View();
+        }
+        public ActionResult Checkout()
+        {
+            return View();
+        }
+        public ActionResult Contact()
+        {
+            return View();
+        }
+        public ActionResult WomenProducts()
+        {
+            return View();
+        }
+        public ActionResult Register()
+        {
+            return View();
+        }
+        public ActionResult Single()
+        {
+            return View();
+        }
+        public ActionResult NewCollection()
+        {
+            return View();
+        }
+        public ActionResult FAQ()
+        {
+            return View();
+        }
+        public ActionResult BestSellers()
+        {
+            return View();
+        }
+        public ActionResult Polarized()
+        {
+            return View();
+        }
+        public ActionResult TopRated()
+        {
+            return View();
+        }
+        public ActionResult OurTeam()
+        {
+            return View();
+        }
+        public ActionResult About()
+        {
+            return View();
+        }
+    }
+}
